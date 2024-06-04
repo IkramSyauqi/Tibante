008d0cc8ae8876c60de9af36af13bcb1ae0a2f27
IkramSyauqi <ikramskei@gmail.com>
2024-06-04 19:22:56 +0700

    edit by ikram

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+ 
+module.exports = merge(common, {
+    mode: 'production',
+    entry: './src/index.js', 
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+});
