43f5c1e990bf5711f67c50b11491682d9326f71e
IkramSyauqi <ikramskei@gmail.com>
2024-06-17 12:25:07 +0700

    edit by ikram

--- src/script/animation/navbar.js
+++ src/script/animation/navbar.js
@@ -1,8 +1,9 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) { 
-        navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-    }
+/* eslint-disable linebreak-style */
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    navbar.classList.remove('navbar-scrolled');
+  }
 });
--- src/script/skipindex.js
+++ src/script/skipindex.js
@@ -1,19 +1,20 @@
+/* eslint-disable linebreak-style */
 document.addEventListener('DOMContentLoaded', () => {
-    const skipLink = document.querySelector('.skip-to-content');
-    
-    skipLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        const targetId = skipLink.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-            targetElement.setAttribute('tabindex', '-1');
-            targetElement.focus();
-        }
-    });
+  const skipLink = document.querySelector('.skip-to-content');
+
+  skipLink.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const targetId = skipLink.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+      targetElement.setAttribute('tabindex', '-1');
+      targetElement.focus();
+    }
+  });
 });
